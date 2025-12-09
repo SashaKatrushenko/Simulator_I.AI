fc45bc60157b1896c53224d8046ba4d0d7996687
SashaKatrushenko <ribeha.ijk@gmail.com>
2025-12-09 20:55:56 +0100

    +csv v lessons

--- Simulator_I.AI/Game.cs
+++ Simulator_I.AI/Game.cs
@@ -10,9 +10,69 @@ namespace Simulator_I.AI
 {
     internal class Game
     {
+        List<Lesson> lessons = new List<Lesson>();
         Random random = new Random();
         public void Start()
-        {
+        {   
+            string fileName = "Book(Sheet1).csv";
+            string[] lines = File.ReadAllLines(fileName);
+            bool prvyRiadok = true;
+
+            foreach (string line in lines)
+            {
+                if (prvyRiadok)
+                {
+                    prvyRiadok = false;
+                    continue;
+                }
+                string[] parts = line.Split(',');
+
+                string den = parts[0];
+                int hodina = int.Parse(parts[1]);
+                string predmet = parts[2];
+                string ucitel = parts[3];
+                int sanceTest = int.Parse(parts[4]);
+                int sanceDu = int.Parse(parts[5]);
+                int sanceNic = int.Parse(parts[6]);
+                int sanceZastup = int.Parse(parts[7]);
+
+                Lesson lesson = new Lesson(den, hodina, predmet, ucitel, sanceTest, sanceDu, sanceNic, sanceZastup);
+                lessons.Add(lesson);
+            }
+
+            string[] dni = { "Pondelok", "Utorok", "Streda", "Stvrtok", "Piatok" };
+
+
+
+
+            foreach (string den in dni)
+            {
+                Console.WriteLine("=== " + den + " ===");
+                for (int hod = 1; hod <= 7; hod++)
+                {
+                    Lesson aktualna = null;
+                    foreach (Lesson l in lessons)
+                    {
+                        if (l.Day == den && l.Hour == hod)
+                        {
+                            aktualna = l;
+                            break;
+                        }
+                    }
+                    if (aktualna == null)
+                    {
+                        Console.WriteLine("Hodina " + hod + "prazdna");
+                        continue;
+                    }
+                    Console.WriteLine("Hodina " + hod + ": " + aktualna.Predmet);
+                }
+            }
+
+
+
+
+            //  Console.WriteLine("lesson nacitane: " + lessons.Count);
+
             Player player = new Player();
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Game start! Po kazdej hodine stracas 5 energie");
@@ -20,6 +80,7 @@ namespace Simulator_I.AI
     
             for (int day = 1; day <= 5; day++)              // Цикл по дням
             {
+                /*
                 player.Energy = 100;
                 player.Mental = 100;
                 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -27,9 +88,13 @@ namespace Simulator_I.AI
                 Console.ResetColor();
                 Console.WriteLine("Player energy: " + player.Energy);
                 Console.WriteLine("Player mental: " + player.Mental);
+                */
+
 
                 for (int hod = 1; hod <= 7; hod++)         // Цикл по часам
                 {
+
+                    /*
                     //Скелет выбора действия, позже добавлю таблицу
                     Console.WriteLine(" HODINA: " + hod);
                     player.ChangeEnergy(-5);
@@ -56,7 +121,7 @@ namespace Simulator_I.AI
                             else
                             {
                                 Console.WriteLine("Dostavas zlu znamku! (-20 mental)");
-                            player.ChangeMental(-20);
+                                player.ChangeMental(-20);
                             }
                         }
 
@@ -70,8 +135,8 @@ namespace Simulator_I.AI
                             }
                             else
                             {
-                                Console.WriteLine("Dostavas zlu znamku! (-15 mental)");
-                                player.ChangeMental(-15);
+                                Console.WriteLine("Dostavas zlu znamku! (-20 mental)");
+                                player.ChangeMental(-20);
                             }
                         }
                     }
@@ -124,6 +189,7 @@ namespace Simulator_I.AI
                     }
                     Console.WriteLine("Player energy: " + player.Energy);
                     Console.WriteLine("Player mental: " + player.Mental);
+                    */
                 }
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine("--- KONIEC DNA " + day + " ---");
@@ -135,7 +201,7 @@ namespace Simulator_I.AI
             }
            
 
-
+          
         }
 
        
--- Simulator_I.AI/Lesson.cs
+++ Simulator_I.AI/Lesson.cs
@@ -8,20 +8,29 @@ namespace Simulator_I.AI
 {
     public class Lesson
     {
-        public string Predmet { get; set; } //nazov predmetu
-        public int ChanceTest { get; set; } //sansa na test
-        public int ChanceDU {  get; set; } //sansa na domacu
-        public int ChanceZastup { get; set; } //sansa zastupu
-        public int ChanceNic { get; set; } //sansa normalna hodina
 
-        public Lesson(string name, int chanceTest, int chanceDU, int chanceNic, int chanceZastup)
-        {
-            Predmet = name;
-            ChanceTest = chanceTest;
-            ChanceDU = chanceDU;
-            ChanceNic = chanceNic;
-            ChanceZastup = chanceZastup;
-        }
+            public string Day { get; set; }       //Pondelok, Utorok, Streda, Štvrtok, Piatok
+        public int Hour { get; set; }            //1-7
+        public string Predmet { get; set; }     //Vyucovaie predmety
+        public string Ucitel { get; set; }     //Meno ucitela
 
+        public int ChanceTest { get; set; }    //Sanca na test v %
+        public int ChanceDU { get; set; }      //Sanca na domacu uciu v %
+        public int ChanceNic { get; set; }     //Sanca na nic v %
+        public int ChanceZastup { get; set; }   //Sanca na zastup v %
+
+        public Lesson(string day, int hour, string predmet, string ucitel,
+                          int chanceTest, int chanceDU, int chanceNic, int chanceZastup)
+            {
+                Day = day;
+                Hour = hour;
+                Predmet = predmet;
+                Ucitel = ucitel;
+                ChanceTest = chanceTest;
+                ChanceDU = chanceDU;
+                ChanceNic = chanceNic;
+                ChanceZastup = chanceZastup;
+            }
     }
+   
 }
